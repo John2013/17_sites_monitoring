a28abaf0e699faf2892a4beff94ad08a9e9c447c
John2013 <NaWasington@yandex.ru>
2018-01-24 20:10:33 +0300

    базовый функционал

--- README.md
+++ README.md
@@ -1,7 +1,38 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+Отображает информацию о сайтах, отвечают ли они и оплачен ли домен на месяц.
 
-# Project Goals
+# Как установить
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+Python 3 должен быть заранее установлен.
+Используйте pip (или pip3 if если есть конфикты с Python 2) чтобы
+установить пакеты:
+
+```bash
+pip install -r requirements.txt # или pip3
+```
+
+Рекомендуется использовать
+[virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/)
+для изоляции пакетов.
+
+# Как использовать
+
+1. Подготовьте файл с ссылками сайтов, разделённые переносами строк,
+например:
+```
+https://ya.ru
+https://google.ru
+http://wbest.org
+```
+
+2. Запустите скрипт:
+
+```bash
+python3 check_sites_health.py <path_to_urls_file>  # или python3
+```
+
+# Цели проекта
+
+Код создан в целях обучения.
+Тренировочныекурсы для web-разработчиков - [DEVMAN.org](https://devman.org)
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,50 @@
+import requests
+import sys
+from os.path import isfile
+from urllib.parse import urlparse
+from datetime import datetime, timedelta
+import whois
+
+
 def load_urls4check(path):
-    pass
+    with open(path) as file:
+        for url in file.read().split('\n'):
+            yield url
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        return requests.get(url).ok
+    except requests.exceptions.ConnectionError:
+        return False
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def get_domain_expiration_date(url):
+    domain = urlparse(url).netloc
+    expiration_date = whois.whois(domain).expiration_date
+    if expiration_date is None:
+        return False
+    one_month = timedelta(hours=24 * 30)
+    return datetime.now() + one_month < expiration_date
+
+
+def print_bool(boolean):
+    if boolean:
+        return '✔'
+    return '✘'
+
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1 and isfile(sys.argv[1]):
+        urls_path = sys.argv[1]
+    else:
+        exit("Файл со ссылками на проверяемые сайты не найден")
+
+    for url in load_urls4check(urls_path):
+        print('url: {}'.format(url))
+        print('http status 200: {}'.format(
+            print_bool(is_server_respond_with_200(url))
+        ))
+        print('Следующий месяц оплачен: {}\n'.format(
+            print_bool(get_domain_expiration_date(url))
+        ))
